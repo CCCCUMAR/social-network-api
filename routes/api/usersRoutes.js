4d6d2e07a2fcd5824843bca6f8fe66318931eb59
Cumar Cumar <umar-19@live.com>
2022-05-29 20:10:35 -0500

    worked coding in the routes

---
+++ routes/api/usersRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const {
+    getUsers, 
+    getOneUser, 
+    createUser, 
+    updateUser, 
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+    router
+        .route('/')
+        .get(getUsers)
+        .post(createUser);
+
+    router
+        .route('/:userId')
+        .get(getOneUser)
+        .put(updateUser)
+        .delete(deleteUser);
+
+    router
+        .route('/:userId/friends')
+        .post(addFriend)
+
+
+    router
+        .route('/:userId/friends/:friendId')
+        .delete(deleteFriend)
+
+
+module.exports = router
